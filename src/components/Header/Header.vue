37b5d56c12c695dbbc979b63eff19debbdbc5f54
Maya <pchelaamaya@gmail.com>
2023-03-03 19:34:51 +0300

    add adaptive header

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,5 +1,6 @@
 <script setup>
 import "./mob.scss"
+import "./desc.scss"
 </script>
 <template>
     <header class="header">
@@ -8,11 +9,22 @@ import "./mob.scss"
                 <a class="header__top--logo" href="/">
                     <img src="@/assets/Header-img/logo.png">
                 </a>
-                <a class="header__top--avatar">
+                <div class="header__search header__search--desc">
+                    <button class="header__search--btn">
+                        <img src="@/assets/Header-img/search.png">
+                    </button>
+                    <input class="header__search--input" type="text" placeholder="Найти...">
+                </div>
+                <a class="header__top--user header__top--user--mobile">
                     <img src="@/assets/Header-img/avatar.png">
                 </a>
+                <a class="header__top--user header__top--user--desc">
+                    <p class="header__top--user--name">Константин</p>
+                    <img src="@/assets/Header-img/avatar_desc.png">
+                </a>
+
             </div>
-            <div class="header__search">
+            <div class="header__search header__search--mobile">
                 <button class="header__search--btn">
                     <img src="@/assets/Header-img/search.png">
                 </button>
